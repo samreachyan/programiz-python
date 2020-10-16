d366f0caccb5d84782f715aee108495297d06151
samreachyan <samreachyan@gmail.com>
2020-10-17 01:44:14 +0700

    basic day1

---
+++ Basic/day1.py
@@ -0,0 +1,23 @@
+import constant
+
+def double(num):
+    '''double any value and return'''
+    # this is comment will be not display on screen
+    return 2 * num
+
+print(constant.PI)
+print(constant.GRAVITY)
+
+print(double.__doc__)
+
+strings = "This is Python"
+char = "C"
+multiline_str = """This is a multiline string with more than one line code."""
+unicode = u"\u00dcnic\u00f6de"
+raw_str = r"raw \n string"
+
+print(strings)
+print(char)
+print(multiline_str)
+print(unicode)
+print(raw_str)
