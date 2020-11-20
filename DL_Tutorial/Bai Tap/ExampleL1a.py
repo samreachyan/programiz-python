6a20dde717ab588db98e4f73c09e7e5c9e43071b
samreachyan <samreachyan@gmail.com>
2020-11-21 01:04:48 +0700

    L1a

---
+++ DL_Tutorial/Bai Tap/ExampleL1a.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.array([[1], [2], [3]])
+b = np.array([[5, 6, 7]])
+
+print(a)
+print(b)
+print('\n')
+print(np.multiply(a, b))
+
+# c = np.array([[1,2,3]])
+# d = np.array([[5], [6], [7]])
+
+# print(c)
+# print(d)
+# print(np.multiply(c, d))
